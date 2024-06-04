4b76e697d817309d1c5847a3782fa278ca584ca5
Héloïse Gaspard <heloise.gaspard@gmail.com>
2024-06-04 14:37:42 +0100

    Added filter so that the DS.stor is not seen as config file

--- test_runner.py
+++ test_runner.py
@@ -25,9 +25,18 @@ if __name__ == '__main__':
     print("Initializing Test Runner")
     os.makedirs(args.logdir, exist_ok=True)
 
+    config_lst = []
     for config in os.listdir(args.configdir):
+        config_lst.append(config)
+
+    config_lst.remove(".DS_Store")  
+    config_lst.remove("Fact_config.config")
+      
+    for config in config_lst:
         print("Running Test with config", config)
         log_subdir = config[:-7]
+        print(config)
+        print(args.configdir)
         os.system("python3 sim.py --config " + os.path.join(args.configdir, config) + " --logdir " + os.path.join(args.logdir,log_subdir) + " --days_per_config 1")
         os.system("python3 plot_gen.py --data " + os.path.join(args.logdir, log_subdir, "sim-summary.pickle"))
 
