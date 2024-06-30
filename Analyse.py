375180861b571b6da41411d3c0c1ab6e524258b5
Héloïse Gaspard <heloise.gaspard@gmail.com>
2024-06-30 15:22:54 +0200

    Log error, h_low and h_high as well as alpha as single values

--- Analyse.py
+++ Analyse.py
@@ -13,20 +13,26 @@ def analyse(filename):
     cdp_axis, txf_axis, run_axis, dai_axis, asset_history, risk_params_axis, herd_params_axis, belief_factor_axis, eth_price_per_day_axis, alpha_axis = pickle.load(input_file)
     data = [cdp_axis, txf_axis, run_axis, dai_axis, asset_history, risk_params_axis, herd_params_axis, belief_factor_axis, eth_price_per_day_axis, alpha_axis]
     
-    print(dai_axis)
     output_dai = np.array(dai_axis[0])
-    print (output_dai)
+
     error = sum((output_dai - real_dai)**2)
+    h_low = np.min(herd_params_axis)
+    h_high = np.max(herd_params_axis)
     
     name = int(random.random()*10000000)
-    print(name)
     log_filename = 'Analyse/'+ str(name) + '_analysis_log.log'
     
-    for value_list in data:
+    log(f'error: {error}', log_filename, flag=True)
+    log(f'alpha: {alpha_axis[0][0]}', log_filename, flag=True )
+        # Log h_low and h_high
+
+    log(f'h_low: {h_low}', log_filename, flag=True)
+    log(f'h_high: {h_high}', log_filename, flag=True)
+    
+    # Log herd_params_axis and alpha_axis
+    for value_list in [dai_axis]:
         for value in value_list:
             log(f'{value}', log_filename, flag=True)
-    
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MAKER Sim Plotter CLI')
